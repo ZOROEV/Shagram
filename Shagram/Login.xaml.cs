c4479a92ea2a7e7455ea3fa1d8947103f66372d0
Syarol <1oleq25@gmail.com>
2018-08-05 19:21:09 +0300

    Added Drop Down menu of choosing country code

--- Shagram/Login.xaml.cs
+++ Shagram/Login.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Input;
+using System.IO;
+using System.Collections.Generic;
 
 using TeleSharp.TL;
 using TLSharp.Core;
+
+using Newtonsoft.Json;
 
 namespace Shagram
 {
@@ -20,7 +24,7 @@ namespace Shagram
         {
             InitializeComponent();        
 
-            var session = new FileSessionStore();
+            FileSessionStore session = new FileSessionStore();
             client = NewClient(session);
             //client.ConnectAsync();
             
@@ -29,6 +33,14 @@ namespace Shagram
                 MainWindow mainWindow = new MainWindow(); // Inicialize main window
                 mainWindow.Show();
                 this.Close();
+            } else
+            {
+                List<Country> countriesList = LoadJson(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "./countries.json");
+                
+                this.countriesCodesList.ItemsSource = countriesList;
+                this.countriesCodesList.DisplayMemberPath = "NameAndCode";
+                this.countriesCodesList.SelectedValuePath = "Dial_code";
+                this.countriesCodesList.SelectedIndex = 0;
             }
         }
 
@@ -68,20 +80,21 @@ namespace Shagram
                 NumberToSendMessage = txt_phone_number.Text;
                 if (string.IsNullOrWhiteSpace(NumberToSendMessage)) return;
 
-                string normalizedNumber;
-                if (!NumberToSendMessage.StartsWith("+"))
+                string normalizedNumber = this.countriesCodesList.SelectedValue.ToString() + NumberToSendMessage;
+                /*if (!NumberToSendMessage.StartsWith("+"))
                 {
                     normalizedNumber = NumberToSendMessage;
                 }
                 else
                 {
                     normalizedNumber = NumberToSendMessage.Substring(1, NumberToSendMessage.Length - 1);
-                }
+                }*/
                 hash = await client.SendCodeRequestAsync(normalizedNumber);
 
                 lbl_phone_number.Visibility = Visibility.Hidden;
                 txt_phone_number.Visibility = Visibility.Hidden;
                 btn_setPhoneNumber.Visibility = Visibility.Hidden;
+                countriesCodesList.Visibility = Visibility.Hidden;
 
                 lbl_received_code.Visibility = Visibility.Visible;
                 txt_received_code.Visibility = Visibility.Visible;
@@ -122,6 +135,32 @@ namespace Shagram
             {
 
             }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public List<Country> LoadJson(string jsonAdress)
+        {
+            //
+            using (StreamReader r = new StreamReader(jsonAdress))
+            {
+                string json = r.ReadToEnd();
+                List<Country> countriesList = JsonConvert.DeserializeObject<List<Country>>(json);
+                return countriesList;
+            }        
+        }
+
+        public class Country
+        {
+            public string Name {get; set; }
+            public string Dial_code { get; set; }
+            public string Code { get; set; }
+            public string Flag { get; set; }
+
+            public string NameAndCode => $"{Name} {Dial_code}";
         }
     }
 }
